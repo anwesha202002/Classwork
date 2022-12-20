e21408d061d272fc7502d72a6328b9adc1b1b2eb
anwesha202002 <121100301+anwesha202002@users.noreply.github.com>
2022-12-20 17:53:44 +0530

    Add files via upload

---
+++ NegetiveOrPositive.java
@@ -0,0 +1,12 @@
+public class NegetiveOrPositive
+{
+public static void main(String[] args)
+{
+   int num=19;
+   if(num<0)
+   System.out.println(num+" is negetive ");
+   else if(num>0)
+   System.out.println(num+" is positive ");
+   else System.out.println(num+" is zero ");
+}
+}
